0011cb4ca8152449ccebd40710adb7bf63468cba
sergio altuzar <sergio.altuzar@gmail.com>
2025-06-07 11:30:39 -0600

    Initial Commit

---
+++ GreenThumb/GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbApp.swift
+//  GreenThumb
+//
+//  Created by Sergio Altuzar on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GreenThumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
