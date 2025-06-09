4767cd01fbdd69d152f4a46cb579a31f7f6e228c
sergio altuzar <sergio.altuzar@gmail.com>
2025-06-09 12:51:34 -0600

    navigation link

--- GreenThumb/Screens/VegetableListScreen.swift
+++ GreenThumb/Screens/VegetableListScreen.swift
@@ -13,7 +13,11 @@ struct VegetableListScreen: View {
     
     var body: some View {
         List(vegetables) { vegetable in
-            VegetableCellView(vegetable: vegetable)
+            NavigationLink {
+                VegetableDetailScreen(vegetable: vegetable)
+            } label: {
+                VegetableCellView(vegetable: vegetable)
+            }
         }
         .listStyle(.plain)
         .task {
